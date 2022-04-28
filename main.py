24365ffcb5ee6468902b5117e97b8aba9faeca22
123 <mralek2017@gmail.com>
2022-04-28 10:07:42 +0500

    third version

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ def main():
     print(f'Hello, my first program')
     print(f'tevirp')
     print(f'321')
+    print(f'b540c95616673f38635d4486080721da1f1050f1')
 if __name__ == '__main__':
     main()
